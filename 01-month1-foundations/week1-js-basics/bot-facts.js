ec1594ccbfc7167999ba4953947367a6b0be6274
MitchiMelon <beginoto.hezatulo@gmail.com>
2026-04-12 20:32:27 +0700

    First coding challenge - Hello

---
+++ 01-month1-foundations/week1-js-basics/bot-facts.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+
+let botName = "KeyMaster";
+let botLocation = "Forged City";
+let favoriteLanguage = "JS";
+
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+
+let codingFact = "Phyton is as hard as " + favoriteLanguage + ".";
+console.log(codingFact);
+codingFact = "However, many said said that " + favoriteLanguage + " is not as enjoyable as Phyton."
+console.log(codingFact)
+codingFact = "Worry not, " + favoriteLanguage + " is begimner friendly and used global-wide."
+console.log(codingFact)
+
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`)
