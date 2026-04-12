70c65c89b10cd861b89627df0fd69a543ee3e8a9
MitchiMelon <beginoto.hezatulo@gmail.com>
2026-04-12 20:38:20 +0700

    js-basics: add mad libs (variables + string concatenation)

---
+++ 01-month1-foundations/week1-js-basics/mad-libs.js
@@ -0,0 +1,25 @@
+let adjective = "pitiful";
+let noun = "cat";
+let verb = "meowing";
+let place = "cape";
+let adjective2 = "mushy";
+let noun2 = "dog";
+
+
+const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+
+console.log(`First story: ${firstStory}`);
+
+adjective = "generous";
+noun = "paladin";
+verb = "playing guitar";
+place = "deep jungle";
+adjective2 = "spikey";
+noun2 = "mushrooms";
+
+
+const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+
+console.log(`Second story: ${secondStory}`);
