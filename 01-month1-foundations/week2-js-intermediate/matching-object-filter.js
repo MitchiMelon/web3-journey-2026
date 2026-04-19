0b08ca214f9a3ae96aebba69719cf6945279287d
MitchiMelon <beginoto.hezatulo@gmail.com>
2026-04-19 21:52:10 +0700

    js-intermediate: add matching object filter (objects + nested loops)

--- 01-month1-foundations/week2-js-intermediate/matching-object-filter.js
+++ 01-month1-foundations/week2-js-intermediate/matching-object-filter.js
@@ -21,33 +21,3 @@ console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercu
 
 
 console.log(whatIsInAName([{"a": 1, "b": 2, "c": 3, "d": 9999}], {"a": 1, "b": 9999, "c": 3}))
-
-Prime Number Sum Calculator
-
-
-function isPrime(n) {
-    if (n < 2) return false;
-    if (n === 2) return true;
-    if (n % 2 === 0) return false;
-    for (let i = 3; i <= Math.sqrt(n); i += 2) {
-        if (n % i === 0) return false;
-    }
-    return true;
-}
-
-
-function sumPrimes(num) {
-    if (num < 2) return 0;
-    let sum = 0;
-    for (let i = 2; i <= num; i++) {
-        if (isPrime(i)) {
-            sum += i;
-        }
-    }
-    return sum;
-}
-
-
-console.log(sumPrimes(10)); // 17
-console.log(sumPrimes(5));  // 10
-console.log(sumPrimes(977)); // 73156
